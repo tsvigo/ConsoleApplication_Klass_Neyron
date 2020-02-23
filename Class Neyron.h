5a798aa3b9ef8cedf06f2c98f8178472c899b7b6
tsvigo <61294618+tsvigo@users.noreply.github.com>
2020-02-23 04:05:38 +0300

    Add files via upload

---
+++ Class Neyron.h
@@ -0,0 +1,17 @@
+#ifndef CLASS NEYRON_H
+#define CLASS NEYRON_H
+
+class Class_Neyron {
+
+private:
+	boolean attribute_Zariad;
+	int attribute_Imia_Ili_Chislo_Neyrona;
+
+public:
+	/**
+	 * Создать экземпляр класса.
+	 */
+	void operation_Sozdaty();
+};
+
+#endif
